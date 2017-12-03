1e28953c7de094fe89aa36c5c9dbec310843a94a
TimBray2 <timbrayy@gmail.com>
2017-12-03 23:07:47 +1300

    Data formatting and distinguishing
    - Data is now displayed in a more presentable matter
    - Variables that need to be accessed have been located
    - Still need to figure out how to make URL parameters send back specific information (If we need to) to help with performance issues

--- apitest.php
+++ apitest.php
@@ -1,52 +1,72 @@
 <?php
 //header('Content-type: Application/JSON');
-/* PRACTICE WITH FAKE DATA
-//echo file_get_html('https://randomuser.me/api/?results=5');
-$file = "https://randomuser.me/api/?results=2";
-$data = file_get_contents($file);
-//$data = mb_substr($data, strpos($data, '{'));
-//$data = mb_substr($data, 0, -1);
-$result = json_decode($data, true);
-$people = $result['results'];
-for ($i = 0; $i < count($people); $i++) {
-  echo "Person " . ($i + 1) . ": " . $people[$i]['gender'] . "<br>";
-}
-$count = 0;
-foreach ($people as $person) {
-  echo "Person " . ($count + 1) . ": " . $people[$count]['gender'] . "<br>";
-  $count++;
-} */
-
 //Retrieve data from PUBG stats
 //setup the request, you can also use CURLOPT_URL
-$ch = curl_init('https://api.pubgtracker.com/v2/profile/pc/JustChillz?region=oc&season=2017-pre5');
-// Returns the data/output as a string instead of raw data
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//Set your auth headers
-$apikey = '1898182c-6af2-4365-b957-382ad3f62e45';
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'TRN-Api-Key: ' . $apikey
-    ));
-// get stringified data/output. See CURLOPT_RETURNTRANSFER
-$data = curl_exec($ch);
-// get info about the request
-$info = curl_getinfo($ch);
-// close curl resource to free up system resources
-curl_close($ch);
-
-//Display the data
+$user = 'JustChillz';
+function getPlayerStats() {
+  $queryString = "?region=oc"; //Need to figure out how to adjust results with more paramaters for performance
+  $ch = curl_init('https://api.pubgtracker.com/v2/profile/pc/' . $GLOBALS['user'] . '/' . $queryString);
+  // Returns the data/output as a string instead of raw data
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  //Set your auth headers
+  $apikey = '1898182c-6af2-4365-b957-382ad3f62e45';
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'TRN-Api-Key: ' . $apikey
+      ));
+  // get stringified data/output. See CURLOPT_RETURNTRANSFER
+  $data = curl_exec($ch);
+  // get info about the request
+  $info = curl_getinfo($ch);
+  // close curl resource to free up system resources
+  curl_close($ch);
+  return $data;
+}
+$data = getPlayerStats();
 /*
 -type e.g. 'stats'
--[0-x] season
+- [0-x] season
 - ['stats']
 - [0 - x] is different stats e.g. 0 = KDR, 1 = winRatio
-*/
 
-/*$result = json_decode($data, JSON_PRETTY_PRINT);
-//print_r($result); use if data is decoded ^^^ */
-echo "<pre>" . $data . "</pre>";
+$result = json_decode($data, JSON_PRETTY_PRINT);
+//print_r($result); //use if data is decoded ^^^
+//echo "<pre>" . $data . "</pre>";
 $section = $result['stats'][0]['stats'];
-for ($i = 0; $i < count($section); $i++) {
-  echo $section[$i]['field'] . ": " . $section[$i]['displayValue'] . "<br>";
+*/
+//Sort using these from user input if not possible to use
+$region = 'oc';
+$season = '2017-pre5';
+
+$result = json_decode($data, JSON_PRETTY_PRINT);
+$section = $result['stats'][0]['stats'];
+//Display the data
+function displayData($result) {
+  echo 'User: ' . $GLOBALS['user'];
+  for ($k = 0; $k < count($result['stats']); $k++) {
+    //sort depending on what they want
+    $section = $result['stats'][$k]['stats'];
+    //print_r($result['stats'][$k]);
+    echo "<br>------------------------------------------------------------------";
+    echo "<h2>" . 'Region: ' . $result['stats'][$k]['region']
+      . " \nMode: " . $result['stats'][$k]['mode']
+      . " \nSeason: " . $result['stats'][$k]['season'] . " </h2>";
+    for ($i = 0; $i < count($section); $i++) {
+      echo $section[$i]['field'] . ": " . $section[$i]['displayValue'] . "<br>";
+    }
+  }
 }
 ?>
+<html>
+<head>
+</head>
+<body>
+  <div id="dataPicker">
+    <p>region / season / mode</p>
+    <!-- selection tools  -->
+  </div>
+  <div id="resultsContent">
+    <h2>Data</h2>
+    <pre id="data"><?php displayData($result); ?></pre>
+  </div>
+</body>
+</html>
