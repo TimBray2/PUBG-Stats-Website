c9b8654f7ab9e460a9bc843524d6ba8553faa7d0
Tim Bray <timbrayy@gmail.com>
2017-12-07 14:24:42 +1300

    React installed locally with video play app working

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,24 @@
 var path = require('path');
 var webpack = require('webpack');
 
-//entry: file that webpack will transform
-var HTMLWebpackPlugin = require('html-webpack-plugin');
-var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + "test.html",
-  filename: 'test.html',
-  inject: 'body'
-});
-
 module.exports = {
-   entry: __dirname + '/js/toDo.js',
-   output: {
-      path:'/',
-      filename: 'transformed.js',
-   },
-   devServer: {
-      inline: true,
-      port: 8080
-   },
-   module: {
-      loaders: [
-         {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-         }
-      ]
-   },
-   plugins: [HTMLWebpackPluginConfig]
-};
+ entry: './js/playerStats.js',
+ output: {
+  path: __dirname,
+  filename: 'js/bundle.js'
+ },
+ watch: true,
+ module: {
+  loaders: [
+   {
+    test: /.jsx?$/,
+    loader: 'babel-loader',
+    exclude: /node_modules/,
+    query: {
+     presets: ['es2015', 'react']
+    }
+   }
+
+  ]
+ },
+};﻿
